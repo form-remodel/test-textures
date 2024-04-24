2896bf4e19f9c9e51239f48992d8c89a8adff50d
Lenin Silva <lenin@formkitchens.com>
2024-04-24 12:18:43 -0600

    feat: add babylon

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import { BabylonScene } from "./components";
+
+function App() {
+  return (
+    <div className="main">
+      <BabylonScene />
+    </div>
+  );
+}
+
+export default App;
